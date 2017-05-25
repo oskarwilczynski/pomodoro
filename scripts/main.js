fc05d69119ad9e754118f9da4b822fbd4ed446fb
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-05-25 22:06:00 +0100

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -18,23 +18,44 @@ const
 work.addEventListener('click', workOn);
 function workOn() {
     mode.innerHTML = this.innerHTML;
-
     minutes.innerHTML = "25";
-    seconds.innerHTML = "00";
-}
-
-shortBreak.addEventListener('click', shortBreakOn);
-function shortBreakOn() {
-    mode.innerHTML = this.innerHTML;
-    
-    minutes.innerHTML = "05";
     seconds.innerHTML = "00";
 }
 
 longBreak.addEventListener('click', longBreakOn);
 function longBreakOn() {
-    mode.innerHTML = this.innerHTML;
-    
+    mode.innerHTML = this.innerHTML;   
     minutes.innerHTML = "15";
     seconds.innerHTML = "00";
 }
+
+shortBreak.addEventListener('click', shortBreakOn);
+function shortBreakOn() {
+    mode.innerHTML = this.innerHTML;  
+    minutes.innerHTML = "05";
+    seconds.innerHTML = "00";
+}
+
+reset.addEventListener('click', function() {
+    work.addEventListener('click', workOn);
+    longBreak.addEventListener('click', longBreakOn);
+    shortBreak.addEventListener('click', shortBreakOn);
+
+    mode.innerHTML = "Work";
+    minutes.innerHTML = "25";
+    seconds.innerHTML = "00";
+});
+
+function leadingZeros(i) {
+    return ("00" + i).slice(-2);
+}
+
+addTime.addEventListener("click", function() {
+    minutes.innerHTML = parseInt(minutes.innerHTML) + 1;
+    minutes.innerHTML = leadingZeros(minutes.innerHTML);
+});
+
+reduceTime.addEventListener("click", function() {
+    minutes.innerHTML = parseInt(minutes.innerHTML) - 1;
+    minutes.innerHTML = leadingZeros(minutes.innerHTML);
+});
