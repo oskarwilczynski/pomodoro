6178cf54d49fb2a61607e357d5999c20bb29672f
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2017-05-30 13:07:33 +0100

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,7 @@ const
     start = document.getElementById("start"),
     resumePause = document.getElementById("resumePause"),
     reset = document.getElementById("reset");
+    alarm = document.getElementById("alarm");
 
 let
     clock;
